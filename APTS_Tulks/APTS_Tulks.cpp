cf6d870c6baf861262b33def416745c2e8f1e9bf
Arturs Klavins <108026286+clavinsh@users.noreply.github.com>
2023-05-05 18:04:37 +0300

    Add project files.

---
+++ APTS_Tulks/APTS_Tulks.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+struct Map {};
+
+
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    // faila 'vārdnīcas' satura ielasīšana iekš Map
+
+    // tālāk noskaidro virzienu --> vai <--
+
+    // tālāk iet pa vardam no fin caur map un beigās izved arā fout (katru vārdu atsevisķi)
+
+    return 0;
+}
+
