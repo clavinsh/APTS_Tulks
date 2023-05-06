17e1be965ff452c4d39d5ec5da4d28ca9e94eeb8
Arturs Klavins <108026286+clavinsh@users.noreply.github.com>
2023-05-06 14:33:34 +0300

    File I/O

--- APTS_Tulks/APTS_Tulks.cpp
+++ APTS_Tulks/APTS_Tulks.cpp
@@ -1,16 +1,42 @@
+//#include <stdio.h>
 #include <iostream>
 
-struct Map {};
+struct Map {
+    struct Node {
+        char key[21];
+        char value[21];
+        Node* next;
+    };
 
 
 
 
+};
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+
+int main() {
+    FILE* input_file = nullptr;
+    FILE* output_file = nullptr;
+
+    if (freopen_s(&input_file, "tulks.in", "r", stdin) != 0) {
+        return -1;
+    }
+
+    if (freopen_s(&output_file, "tulks.out", "w", stdout) != 0) {
+        return -1;
+    }
+
 
     // faila 'vārdnīcas' satura ielasīšana iekš Map
+
+    char currentString[21];
+
+    while (!std::feof(input_file)) {
+        scanf_s("%s", currentString, (unsigned)_countof(currentString));
+        printf(currentString);
+    }
+
 
     // tālāk noskaidro virzienu --> vai <--
 
