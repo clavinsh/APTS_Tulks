1e9df1a5201adc5f992425f08105fed21148d151
Arturs Klavins <108026286+clavinsh@users.noreply.github.com>
2023-05-07 20:25:31 +0300

    code structure cleanup

--- APTS_Tulks/APTS_Tulks.cpp
+++ APTS_Tulks/APTS_Tulks.cpp
@@ -1,77 +1,77 @@
-#include <stdio.h>
+#include <fstream>
 
 const int MAX_CSTRING_SIZE = 21;
 const float LOAD_FACTOR = 0.75;
+const int DEFAULT_CAPACITY = 16*16*16*16;
 
-size_t strlen(const char* str) {
-    size_t size = 0;
-    while (*str != '\0') {
-        ++size;
-        ++str;
-    }
-    return size;
-}
-
-int strcmp(const char* str1, const char* str2) {
-    while (*str1 != '\0' && *str2 != '\0') {
-        if (*str1 != *str2) {
-            return (*str1 < *str2) ? -1 : 1;
+namespace helpers {
+    size_t strlen(const char* str) {
+        size_t size = 0;
+        while (*str != '\0') {
+            ++size;
+            ++str;
         }
-        str1++;
-        str2++;
+        return size;
     }
-    return (*str1 == '\0' && *str2 == '\0') ? 0 : ((*str1 == '\0') ? -1 : 1);
-}
 
-void* memset(void* ptr, int value, size_t num) {
-    unsigned char* p = static_cast<unsigned char*>(ptr);
-    for (size_t i = 0; i < num; i++) {
-        p[i] = static_cast<unsigned char>(value);
-    }
-    return ptr;
-}
-
-char* strncpy(char* dest, const char* src, size_t n) {
-    char* dest_ptr = dest;
-    const char* src_ptr = src;
-
-    // Copy at most n characters
-    for (size_t i = 0; i < n; ++i) {
-        if (*src_ptr != '\0') {
-            *dest_ptr = *src_ptr;
-            ++src_ptr;
+    int strcmp(const char* str1, const char* str2) {
+        while (*str1 != '\0' && *str2 != '\0') {
+            if (*str1 != *str2) {
+                return (*str1 < *str2) ? -1 : 1;
+            }
+            str1++;
+            str2++;
         }
-        else {
-            // Pad destination with null characters
-            *dest_ptr = '\0';
-        }
-        ++dest_ptr;
+        return (*str1 == '\0' && *str2 == '\0') ? 0 : ((*str1 == '\0') ? -1 : 1);
     }
 
-    return dest;
+    void* memset(void* ptr, int value, size_t num) {
+        unsigned char* p = static_cast<unsigned char*>(ptr);
+        for (size_t i = 0; i < num; i++) {
+            p[i] = static_cast<unsigned char>(value);
+        }
+        return ptr;
+    }
+
+    char* strncpy(char* dest, const char* src, size_t n) {
+        char* dest_ptr = dest;
+        const char* src_ptr = src;
+
+        // Copy at most n characters
+        for (size_t i = 0; i < n; ++i) {
+            if (*src_ptr != '\0') {
+                *dest_ptr = *src_ptr;
+                ++src_ptr;
+            }
+            else {
+                // Pad destination with null characters
+                *dest_ptr = '\0';
+            }
+            ++dest_ptr;
+        }
+
+        return dest;
+    }
 }
 
 class Map {
 private:
-    static const int DEFAULT_CAPACITY = 16;
-
     struct Node {
         char key[MAX_CSTRING_SIZE];
         char value[MAX_CSTRING_SIZE];
-        //int count;
         Node* next;
         size_t hashValue;
     };
 
     Node** buckets;
-    int size;
-    int capacity;
-
+    size_t size;
+    size_t capacity;
+    
     // hash function for bucket determination
     // ensures constant lookup, insert, delete if no collisions 
     size_t hash(const char* key) {
         size_t hValue = 5381;
-        int length = strlen(key);
+        size_t length = helpers::strlen(key);
         for (int i = 0; i < length; i++) {
             hValue = ((hValue << 5) + hValue) + key[i];
         }
@@ -82,15 +82,15 @@ private:
     // copies the contents of map into a resized array, if the used space exceeds load factor
     void resize() {
         if (size >= LOAD_FACTOR * capacity) {
-            int newCapacity = capacity * 2;
+            size_t newCapacity = capacity * 2;
             Node** newBuckets = new Node * [newCapacity];
 
-            memset(newBuckets, 0, newCapacity * sizeof(Node*));
+            helpers::memset(newBuckets, 0, newCapacity * sizeof(Node*));
 
             for (int i = 0; i < capacity; i++) {
                 Node* node = buckets[i];
                 if (node != nullptr) {
-                    int index = node->hashValue % newCapacity;
+                    size_t index = node->hashValue % newCapacity;
                     newBuckets[index] = node;
                 }
             }
@@ -101,11 +101,11 @@ private:
         }
     }
 
-    void setBuckets(Node** invBuckets, int invCapacity, int invSize) {
+    void setBuckets(Node** invBuckets, size_t invCapacity, size_t invSize) {
         clear();
 
         buckets = new Node * [invCapacity];
-        memset(buckets, 0, invCapacity * sizeof(Node*));
+        helpers::memset(buckets, 0, invCapacity * sizeof(Node*));
 
         for (int i = 0; i < invCapacity; i++) {
             Node* invNode = invBuckets[i];
@@ -114,8 +114,8 @@ private:
 
             while (invNode != nullptr) {
                 newNode = new Node();
-                strncpy(newNode->key, invNode->key, MAX_CSTRING_SIZE);
-                strncpy(newNode->value, invNode->value, MAX_CSTRING_SIZE);
+                helpers::strncpy(newNode->key, invNode->key, MAX_CSTRING_SIZE);
+                helpers::strncpy(newNode->value, invNode->value, MAX_CSTRING_SIZE);
                 newNode->hashValue = invNode->hashValue;
                 newNode->next = nullptr;
 
@@ -138,7 +138,7 @@ private:
 public:
     Map() : size(0), capacity(DEFAULT_CAPACITY) {
         buckets = new Node * [capacity];
-        memset(buckets, 0, capacity * sizeof(Node*));
+        helpers::memset(buckets, 0, capacity * sizeof(Node*));
     }
 
     ~Map() {
@@ -159,15 +159,15 @@ public:
         resize();
 
         size_t hashValue = hash(key);
-        int index = hashValue % capacity;
+        size_t index = hashValue % capacity;
 
         Node* node = buckets[index];
 
         if (node == nullptr) {
             Node* newNode = new Node();
 
-            strncpy(newNode->key, key, MAX_CSTRING_SIZE);
-            strncpy(newNode->value, value, MAX_CSTRING_SIZE);
+            helpers::strncpy(newNode->key, key, MAX_CSTRING_SIZE);
+            helpers::strncpy(newNode->value, value, MAX_CSTRING_SIZE);
 
             newNode->hashValue = hashValue;
             buckets[index] = newNode;
@@ -181,8 +181,8 @@ public:
         }
 
         Node* newNode = new Node();
-        strncpy(newNode->key, key, MAX_CSTRING_SIZE);
-        strncpy(newNode->value, value, MAX_CSTRING_SIZE);
+        helpers::strncpy(newNode->key, key, MAX_CSTRING_SIZE);
+        helpers::strncpy(newNode->value, value, MAX_CSTRING_SIZE);
         newNode->hashValue = hashValue;
         node->next = newNode;
         size++;
@@ -192,12 +192,12 @@ public:
 
     const char* get(const char* key) {
         size_t hashValue = hash(key);
-        int index = hashValue % capacity;
+        size_t index = hashValue % capacity;
 
         Node* node = buckets[index];
 
         while (node != nullptr) {
-            if (strcmp(node->key, key) == 0) {
+            if (helpers::strcmp(node->key, key) == 0) {
                 return node->value;
             }
             node = node->next;
@@ -205,7 +205,6 @@ public:
 
         return nullptr;
     }
-
 
     void invert() {
         Map* inverted = new Map();
@@ -236,7 +235,6 @@ int main() {
     scanf("%s", &currentString);
 
     // putting the contents of the file into Map
-    // || strcmp(currentString, "<--") != 0 || strcmp(currentString, "-->") != 0
     while ((currentString[0] != '-') && (currentString[0] != '<')) {
         char val[MAX_CSTRING_SIZE];
 
@@ -250,11 +248,11 @@ int main() {
     // the direction ('<--' or '-->') signifies whether to following text from file will need to be found/replaced by
     // key or value (essentially should the keys and values should be logically swapped in the map)
     //bool direction = currentString[0] == '-' ? false : true;
-
     if (currentString[0] == '<') {
         map->invert();
     }
 
+    // writing to output file
     bool lastString = false;
     while (scanf("%s", currentString) != EOF) {
         const char* value = map->get(currentString);
